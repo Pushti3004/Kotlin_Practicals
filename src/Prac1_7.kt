ad0aa16a96a704fefafbccf8fcbf361ebde8982f
Pushti Gandhi <shreejipdg@gmail.com>
2026-07-12 16:23:34 +0530

    init

--- src/Prac1_7.kt
+++ src/Prac1_7.kt
@@ -1,12 +1,20 @@
-fun factorial(n: Int): Int {
+fun factorial0(n: Int): Int {
     if (n == 0 || n == 1)
         return 1
     else
-        return n * factorial(n - 1)
+        return n * factorial0(n - 1)
+}
+tailrec fun factorial1(n: Int, result: Long = 1): Long {
+    return if (n == 0 || n == 1)
+        result
+    else
+        factorial1(n - 1, result * n)
 }
 
 fun main() {
     print("Enter Number:")
     val num = readln().toInt()
-    println("Factorial of $num = "+factorial(num))
+    println("Factorial of $num = "+factorial0(num))
+    println("By TailRec Keyword, Factorial of $num = "+factorial1(num))
+
 }
